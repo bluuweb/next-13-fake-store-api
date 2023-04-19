90c552e152eea4f1c87ca833ad1f0a06afc10d61
bluuweb <contacto@bluuweb.cl>
2023-04-19 07:15:24 -0400

    dia 1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    // sirve pero es más genérico
+    // domains: ["fakestoreapi.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        // opcionales pero con mayor seguridad
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
